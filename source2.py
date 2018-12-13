c1e481de0013b8b82c107d6a5383c786c822244d
mono-sama <38932852+mono-sama@users.noreply.github.com>
2018-12-13 04:50:50 +0000

    Update source2.py

--- source2.py
+++ source2.py
@@ -266,7 +266,7 @@ Want to add me elsewhere? Sure thing!
             
     if message.content.startswith("r!ghost"):
         args = message.content.split(" ")
-        await client.send_message(discord.Object(id='505410274807906304'), "%s" % (" ".join(args[1:])))
+        await client.send_message(discord.Object(id='460772285163241474'), "%s" % (" ".join(args[1:])))
         
         
     if message.content.startswith("r!help"):
